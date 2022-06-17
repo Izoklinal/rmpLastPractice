0b0ab9a11d016ddc48bd38eb3861cd56919a3491
akumu999 <snow.lite@mail.ru>
2022-06-17 11:33:28 +0500

    v2_three_working_buttons

--- app/src/main/java/com/bignerdranch/android/book_app/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/book_app/MainActivity.kt
@@ -3,25 +3,61 @@ package com.bignerdranch.android.book_app
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast,false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia,true)
+    )
+    private var currentIndex = 0
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.button_false)
 
-        trueButton.setOnClickListener {
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
-            Log.d("try", "correct")
+            trueButton = findViewById(R.id.true_button)
+            falseButton = findViewById(R.id.button_false)
+            nextButton = findViewById(R.id.next_button)
+            questionTextView = findViewById(R.id.question_text_view)
+
+            trueButton.setOnClickListener {
+                checkAnswer(true)
+                Log.d("try", "true")
+            }
+            falseButton.setOnClickListener {
+                checkAnswer(false)
+                Log.d("try", "false")
+            }
+            nextButton.setOnClickListener {
+                currentIndex = (currentIndex+1)%questionBank.size
+                updateQuestion()
+            }
+            updateQuestion()
         }
-        falseButton.setOnClickListener {
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
-            Log.d("try", "incorrect")
-        }
-    }
 }
